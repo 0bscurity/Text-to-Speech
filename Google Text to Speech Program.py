f972cd6d2228b4e6edb688f722a5d43f8c4d09d7
Andrew Hawk <54362839+0bscurity@users.noreply.github.com>
2022-05-13 12:38:10 -0400

    Update Google Text to Speech Program.py
    Fixed menu options and reading from txt file.

--- Google Text to Speech Program.py
+++ Google Text to Speech Program.py
@@ -2,23 +2,23 @@ from gtts import gTTS
 
 menu = int(input('Choose menu option\n1. text input\n2. .txt import\n3. Quit\n~ '))
 
-if menu == 1:
-    usrtxt = str(input('Enter the text you would like to convert to speech: \n'))
-    tts = gTTS(text = usrtxt, lang = 'en')
-    tts.save("speech_file_1.mp3")
-    print("****************************************************\n" + usrtxt + "\
- was exported to " + "filename\n*****************\
-***********************************")
-elif menu == 2:
-    f = open(input('Enter filename: '))
-    usrtxt = f.read()
-    tts = gTTS(text = usrtxt, lang = 'en')
-    tts.save("speech_file_1.mp3")
-    f.close()
-elif menu == 3:
-    quit
-
-addconv = str(input('To make another conversion, press "a" then return\n'))
-
-if addconv == 'a':
+while menu != 3:
+    if menu == 1:
+        usrtxt = str(input('Enter the text you would like to convert to speech: \n'))
+        tts = gTTS(text = usrtxt, lang = 'en')
+        tts.save("speech_file.mp3")
+        print("****************************************************\n" + usrtxt + "\
+     was exported to " + "speech_file.mp3\n*****************\
+    ***********************************")
+    elif menu == 2:
+        f = open(input('Enter filename: '))
+        usrtxt = f.read()
+        tts = gTTS(text = usrtxt, lang = 'en')
+        tts.save("speech_file_1.mp3")
+        print("****************************************************\n" + f + "\
+         was exported to " + "speech_file.mp3\n*****************\
+        ***********************************")
+        f.close()
+    elif menu == 3:
+        quit
     menu = int(input('Choose menu option\n1. text input\n2. .txt import\n3. Quit\n~ '))
